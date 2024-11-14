8a5481b21e4700386581bc575a444d8153dae4d4
vitaliymv <movcan77@gmail.com>
2024-11-14 19:35:36 +0200

    Add 2 request to card banking api

--- script.js
+++ script.js
@@ -1,51 +1,16 @@
-let types = ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
+const ownerKey = "kfdduhdrmcjvfjkfud"
+const url = "https://thread-apricot-index.glitch.me/cards/"
 
-$.each(types, (index, type) => {
-    $("#types").append(`
-        <label for="${type}">${type.toUpperCase()}</label>
-        <input type="radio" id="${type}" value="${type}" name="type">   
-    `)
+fetch(url + ownerKey).then(async function (response) {
+    console.log(await response.json());
 })
 
-$("#sliderText").text($("#members").val());
-$("#members").on("input", function() {
-    $("#sliderText").text($(this).val());
-})
-
-$("#rnd-act-btn").click(function () {
-    let params = $("#filters").serialize();
-    $(this).attr("disabled", true);
-    $.ajax(`http://bored.api.lewagon.com/api/activity/?${params}`, {
-        success: (result) => {
-            $("#activity").show();
-            if (result.error) {
-                $("#activity").html(`<h3>${result.error}</h3>`)
-            } else {
-                $("#activity").html(`
-                    <h3>${result.activity}</h3>    
-                    <p>Type: ${result.type}</p>
-                    <p>Participants: ${result.participants}</p>
-                    <p>Price: ${result.price}</p>
-                    <p>Accessibility: 
-                        <span class="${getColor(result.accessibility)}">
-                            ${result.accessibility}
-                        </span>
-                    </p>
-                `);
-                if (result.link) {
-                    $("#activity").append(`
-                        <a href="${result.link}">Link</a>
-                    `)
-                }
-            }
-            $(this).attr("disabled", false);
-            $(this).textx("Raandomize again")
-        }
+document.querySelector("#new").addEventListener("click", () => {
+    fetch(url, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({"ownerKey": ownerKey})
+    }).then(async function (response) {
+        console.log(await response.json());
     })
 })
-
-function getColor(value) {
-    if (value >= 0.0 && value <= 0.39) { return "green" }
-    else if (value >= 0.4 && value <= 0.69) { return "gold" }
-    else { return "red" }
-}
