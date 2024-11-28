f7491a225bfee872410bf471ee381d07cb0caad3
vitaliymv <movcan77@gmail.com>
2024-11-28 19:12:19 +0200

    Add put request, some fix

--- script.js
+++ script.js
@@ -31,9 +31,12 @@ function render(cards) {
                 <td>${formattedNumber}</td>
                 <td>${card.expireDate}</td>
                 <td>${card.balance}₴</td>
-                <td><input type="password" value="${card.cvv}" readonly></td>
+                <td class="position-relative">
+                    <input type="password" value="${card.cvv}" readonly id="cvv-input">
+                    <i class="fa-solid fa-eye fa-lg position-absolute top-50 ms-3" style="cursor: pointer;"></i>
+                </td>
                 <td>
-                    <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <button onclick="setNumber('${card.cardNumber}')" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
                         <i class="fa-solid fa-pen-to-square"></i>
                     </button>
                 </td>
@@ -47,10 +50,37 @@ function render(cards) {
     }
 }
 
+let hiddenNumber = document.querySelector("#card-number");
+function setNumber(number) {
+    hiddenNumber.value = number;
+}
+
+let form = document.querySelector("#form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let number = document.querySelector("#card-number").value;
+    let needRadio = Array.from(event.target["type"]).find(radio => radio.checked);
+    let amount = event.target["amount"].value;
+    if (amount < 0) amount = Math.abs(amount);
+    if (needRadio.id == "withdraw") amount *= -1;
+
+    fetch(url + number + "/balance", {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({"amount": amount})
+    }).then(async function (response) {
+        getData();
+    })    
+})
+
 function deleteCard(number) {
     fetch(url + number, {
         method: "DELETE"
     }).then(async function (response) {
+        let data = await response.json();
+        if (response.status == 403) return alert(data.error)
         getData();
+        alert(data.message)
     })
 }
+
